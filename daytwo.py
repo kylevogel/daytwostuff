6c1c3eaf8c669073b339ce1c85d2c542967ee3e4
Kyle Vogel <xbu7xh@virginia.edu>
2026-01-15 20:00:50 +0000

    Updated project

---
+++ daytwo.py
@@ -0,0 +1,17 @@
+#%%
+# /workspaces/daytwostuff
+
+#%%
+# python3 -m venv .venv
+
+#%%
+# Yes to ensure it follows requirements.txt
+
+#%%
+# /workspaces/daytwostuff
+# No it isn't different
+
+#%%
+import pandas as pd
+data = pd.read_csv('pitching-sample.csv')
+print(data.head())
